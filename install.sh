81378da6f7ea1d1ed203d61cd2021944d0b20083
KiamMota <somentekiam@gmail.com>
2026-08-22 11:32:59 -0300

    add install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,25 @@
 
 set -e
 
+REPO="https://github.com/kiammota/klib.git"
+DEST="/tmp/klib-install"
+
+echo "Cloning klib..."
+git clone --depth=1 "$REPO" "$DEST"
+
+cd "$DEST"
+
 mkdir -p build
 cd build
+
+echo "Building..."
 cmake ..
 make
+
+echo "Installing..."
 sudo make install
+
+cd /
+rm -rf "$DEST"
+
+echo "Done."
